4e2f9aa54de1fde7742d882fb43ecdf50346ac31
heikelhalim <48664086+heikelhalim@users.noreply.github.com>
2021-09-22 11:20:22 +0800

    tukar skit server

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(cors(corsOptions));
  
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Maroz application." });
 });
 
   
