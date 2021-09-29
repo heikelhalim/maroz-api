36d2b16d621ed307f5cc791317ef9195e735d267
heikelhalim <48664086+heikelhalim@users.noreply.github.com>
2021-09-29 10:17:38 +0800

    auth middleware

--- source/usingDB/routes/syarikat/Syarikat.js
+++ source/usingDB/routes/syarikat/Syarikat.js
@@ -1,11 +1,11 @@
 const express = require("express");
 var router = express.Router();
-
+const Auth = require("../../middleware/Auth");
 
 const Syarikat = require("../../controller/syarikat/Syarikat");
 
-router.post('/syarikat/create',  Syarikat.create);
-router.post('/syarikat/list',  Syarikat.getList);
-router.get('/syarikat/view/:id',  Syarikat.getDetails);
+router.post('/syarikat/create', Auth.verifyToken,  Syarikat.create);
+router.post('/syarikat/list', Auth.verifyToken,  Syarikat.getList);
+router.get('/syarikat/view/:id', Auth.verifyToken,  Syarikat.getDetails);
  
  module.exports = router;
