62a6b66c0b8cad2b6b142dd15199d19acff659b1
heikelhalim <48664086+heikelhalim@users.noreply.github.com>
2021-10-18 03:39:59 +0800

    betul errok path tak jumpa benda ntah

--- source/usingDB/routes/staff/Staff.js
+++ source/usingDB/routes/staff/Staff.js
@@ -1,8 +1,10 @@
 
 const express = require("express");
 var router = express.Router();
+
 const Auth = require("../../middleware/Auth");
-const Staff = require("../../controller/staff/Staff"); 
+const Staff = require("../../controller/staff/Staff");
+
 
 router.post('/staff/list', Auth.verifyToken, Staff.getListStaff);
 router.post('/staff/create', Auth.verifyToken, Staff.creatNonSystemUser);
